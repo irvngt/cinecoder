78e8c4f9c0644c25fa737e75862363b929ca0d9f
irvngt <irving.mendoza0409@gmail.com>
2026-08-13 15:52:06 -0600

    Mejoras al theme con gpt5 mini

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,13 @@
+<?php
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor')
+    ->name('*.php');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder);
--- index.php
+++ index.php
@@ -1,25 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <?php  wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?></title>
+    <?php wp_head(); ?>
 </head>
-<body>
-    <h1>hola miguel</h1>
+<body <?php body_class(); ?>>
+    <a class="skip-link" href="#main-content"><?php esc_html_e( 'Saltar al contenido', 'cinecoder' ); ?></a>
 
-    <li><a href="http://">ADIOS Luis</a></li>
-    <li><a href="http://">ADIOS Luis</a></li>
-    <li><a href="http://">ADIOS Luis</a></li>
-    <li><a href="http://">ADIOS Luis</a></li>
-    <li><a href="http://"><?php _e('hola Luis Negrete','cinecoder'); ?></a></li>
-    <p><?php _e('Fuckoff','cinecoder'); ?></p>
-    <p>
-    <?php  
-    echo __('Adios Luis','cinecoder'); ?>
-    </p>
-<?php  wp_footer(); ?>
+    <header>
+        <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Hola Miguel', 'cinecoder' ); ?></a></h1>
+    </header>
+
+    <nav role="navigation" aria-label="<?php esc_attr_e( 'Menú principal', 'cinecoder' ); ?>">
+        <ul>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Inicio', 'cinecoder' ); ?></a></li>
+            <li><a href="#"><?php esc_html_e( 'Acerca', 'cinecoder' ); ?></a></li>
+            <li><a href="#"><?php esc_html_e( 'Contacto', 'cinecoder' ); ?></a></li>
+            <li><a href="#"><?php echo esc_html__( 'Adiós Luis', 'cinecoder' ); ?></a></li>
+            <li><a href="#"><?php echo esc_html__( 'Hola Luis Negrete', 'cinecoder' ); ?></a></li>
+        </ul>
+    </nav>
+
+    <main id="main-content" tabindex="-1">
+        <p><?php esc_html_e( 'Adiós Luis', 'cinecoder' ); ?></p>
+    </main>
+
+    <?php wp_footer(); ?>
 </body>
 </html>
